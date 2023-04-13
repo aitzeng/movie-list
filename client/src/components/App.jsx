c1da20b5771f532e3ad3b9185f4f12b9182fbb12
aitzeng <aitzeng@gmail.com>
2023-04-12 23:33:07 -0700

    Update App

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,27 +2,53 @@ import React from 'react';
 import MovieList from './MovieList.jsx';
 import movies from '../ExampleData/ExampleData.js';
 import Search from './Search.jsx';
+import AddMovie from './AddMovie.jsx';
 // import SubmitButton from './SubmitButton.jsx';
 
 const { useState } = React;
 
 
+
+
 const App = () => {
 
+  // Sets the current state of the Search Input. Current state will be set to "", but as the value changes, it will update the state to reflect it.
   const [searchInput, setSearchInput] = useState("");
+  // Setting the Movies to also have a state. This is because as we filter based on searchInput, the allMovies table will update as well
+  const [allMovies, setAllMovies] = useState(movies);
+
+  const [initialInput, addInput] = useState("")
+  // const [currentFilter, setFilter] = useState("");
+  // Filters the movies based on the searchInput state
+  const filteringMovies = function(e) {
+    // Prevents the page from refreshing after each submit
+    e.preventDefault();
+    if (searchInput === "") {
+      setAllMovies(movies)
+    } else {
+      var filteredMovies = allMovies.filter(movie => movie.title.toLowerCase().includes(searchInput.toLowerCase()))
+      setAllMovies(filteredMovies);
+      setSearchInput("")
+    }
+  }
+
+  const addingMovies = function(movieString) {
+    var addedMovie = [...movies, {title: movieString} ]
+    setAllMovies(addedMovie);
+  }
 
   return (
 
     <div>
-      <form className="formbar">
-        <Search search={searchInput} setSearchInput={setSearchInput}/>
-        {/* <SubmitButton /> */}
-        {/* Before, I only had search={searchInput}. This only passes in the searchInput state and not the setSearchInput function */}
-        {/* <button className="searchButton">Press Me</button> */}
-      </form>
+      <AddMovie input={initialInput} addInput={addInput} addingMovies={addingMovies}/>
+      <Search search={searchInput} setSearchInput={setSearchInput} filteringMovies={filteringMovies}/>
+      {/* <SubmitButton /> */}
+      {/* Before, I only had search={searchInput}. This only passes in the searchInput state and not the setSearchInput function */}
+      {/* <button className="searchButton">Press Me</button> */}
       <div>Movie List</div>
       <table className="movieTable">
-        <MovieList movies = {movies} />
+        {/* Sets up rows based on current state of allMovies */}
+        <MovieList movies = {allMovies}/>
       </table>
     </div>
   );
