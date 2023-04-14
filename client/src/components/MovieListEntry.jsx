895f07acf93c6d3e6694aa7b92ed40edac587b99
aitzeng <aitzeng@gmail.com>
2023-04-13 18:56:08 -0700

    Updated to include toggle feature

--- client/src/components/MovieListEntry.jsx
+++ client/src/components/MovieListEntry.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
 
+const { useState, useEffect } = React;
+
 const MovieListEntry = ( {movie} ) => {
 
+  const [watched, setWatched] = useState("To Watch")
+
+  var handleClick = function(e) {
+    e.preventDefault();
+    if (watched === "To Watch") {
+      setWatched("Watched");
+    } else {
+      setWatched("To Watch")
+    }
+  }
 
   return (
-    <tr>
+    <tr onClick={handleClick}>
       <td>{movie.title}</td>
+      <td>{watched}</td>
     </tr>
   )
 
