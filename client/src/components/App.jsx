9c0526ed436937fff10fcbef5a9eb1b2ac64f3f6
aitzeng <aitzeng@gmail.com>
2023-04-21 15:35:14 -0700

    Added watch toggle and buttons

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -10,48 +10,60 @@ const { useState, useEffect } = React;
 
 const App = () => {
 
-  // Sets the current state of the Search Input. Current state will be set to "", but as the value changes, it will update the state to reflect it.
-  const [searchInput, setSearchInput] = useState("");
-  // Setting the Movies to also have a state. This is because as we filter based on searchInput, the allMovies table will update as well
   const [allMovies, setAllMovies] = useState([]); //Change to [] when want to have no example data
   const [input, setInput] = useState("")
-  const [filteredMovies, setFilteredMovies] = useState(allMovies)
+  // Sets the current state of the Search Input. Current state will be set to "", but as the value changes, it will update the state to reflect it.
+  const [searchInput, setSearchInput] = useState("");
+  const [filteredMovies, setFilteredMovies] = useState(allMovies);
+  const [before, setBefore] = useState(filteredMovies);
+  // Setting the Movies to also have a state. This is because as we filter based on searchInput, the allMovies table will update as well
+
+  // const [currentList, setcurrentList] = useState(toWatch)
+
   // const [watched, togglewatched] = useState("To Watch")
   // const [currentFilter, setFilter] = useState("");
   // Filters the movies based on the searchInput state
 
   // This will change it so that everytime allMovies state changes, my filteredMovies is updated to it.
+
+  const addingMovies = function() {
+    var addedMovie = [...allMovies, {title: input, watched: "To Watch"} ]
+    setAllMovies(addedMovie);
+  }
+
   useEffect(() => {
     setFilteredMovies(allMovies);
   }, [allMovies]);
 
-  const watchFilter = function() {
-    const watchedAlready = filteredMovies.filter(movie => movie.watched.includes("Watched"));
-    setFilteredMovies(watchedAlready);
-  }
+  useEffect(() => {
+    setBefore(filteredMovies);
+  }, [filteredMovies])
+
+  // const watchedFilter = function(movie) {
+  //   setwatchedState([...watchedState, movie])
+  //   setToWatch(toWatch.filter(m => m.title !== movie.title));
+  // }
+  // const toWatchFilter = function(movie) {
+  //   setToWatch([...toWatch, movie]);
+  //   setwatchedState(watchedState.filter(m => m.title !== movie.title))
+  // }
 
   //This function does not modify the allMovies but the filtered data
   const filteringMovies = function(e) {
     // Prevents the page from refreshing after each submit
     e.preventDefault();
-    // if (searchInput === "") {
-    //   setFilteredMovies()
-    // } else {
     const filtered = allMovies.filter(movie => movie.title.toLowerCase().includes(searchInput.toLowerCase()))
     setFilteredMovies(filtered);
     setSearchInput("")
+    console.log('This is allMovies:', allMovies)
   }
-  const addingMovies = function() {
-    var addedMovie = [...allMovies, {title: input} ]
-    setAllMovies(addedMovie);
-  }
-
 
   // useEffect takes an anonymous function and a state. If it notices the state has changed, it will invoke the anonymous function
   // useEffect(() => {
   //   console.log(input);
   // }, [input] )
 
+  // Try setting states for the watched/to watch to an array. May need two different states and two different arrays.
   return (
 
     <div>
@@ -61,10 +73,11 @@ const App = () => {
       {/* <SubmitButton /> */}
       {/* Before, I only had search={searchInput}. This only passes in the searchInput state and not the setSearchInput function */}
       {/* <button className="searchButton">Press Me</button> */}
-      <ToggleWatch movies={filteredMovies} watchFilter={watchFilter}/>
+      {/* On click for the toggleWatch, I want it to filter the rows that have "To Watch" or "Watched"*/}
+      <ToggleWatch movies={filteredMovies} setBefore={setBefore}/>
       <table className="movieTable">
         {/* Sets up rows based on current state of allMovies */}
-        <MovieList movies = {filteredMovies}/>
+        <MovieList movies = {before}/>
       </table>
     </div>
   );
