98edd322cc004b42f239098939bfdcbd05788706
aitzeng <aitzeng@gmail.com>
2023-04-22 22:07:24 -0700

    Created example data

---
+++ server/db/exampleData.js
@@ -0,0 +1,9 @@
+var movies = [
+  {title: 'Mean Girls', status: 'Watched'},
+  {title: 'Hackers', status: 'To Watch'},
+  {title: 'The Grey', status: 'Watched'},
+  {title: 'Sunshine', status: 'Watched'},
+  {title: 'Ex Machina', status: 'To Watch'},
+];
+
+// modules.exports = movies;
